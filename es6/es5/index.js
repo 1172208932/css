a8120377aa389117383fc407777e108db57b3e89
1172208932 <1172208932@qq.com>
2018-04-01 15:40:16 +0800

    es6

---
+++ es6/es5/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+        var oDiv = document.querySelector("#odiv");
+        var a = 5;
+        var b = 7;
+        var str = "<ul>\n    <li>d" + a + "+" + b + "=" + (a + b) + "d</li>\n    </ul>";
+        oDiv.innerHTML = str;
---
+++ es6/es6.js
@@ -0,0 +1,7 @@
+let oDiv =document.querySelector("#odiv");
+    let a =5;
+    let b = 7;
+    let str =`<ul>
+    <li>d${a}+${b}=${a+b}d</li>
+    </ul>`;
+    oDiv.innerHTML = str;
